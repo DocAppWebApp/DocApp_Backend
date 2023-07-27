70af551aee5ee5a70c39fb109f6841f01f297ddd
Jorge Fergusson <jorgefergusson@gmail.com>
2023-07-27 19:49:12 -0400

    Delete initial content to start from the example in class

--- config/dbconfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "ATLASDB": "mongodb+srv://docapcentennialcollege:docapmongodb@docappdatabase.e3o614w.mongodb.net/?retryWrites=true&w=majority",
-    "SECRETKEY": "LoveMongoDB@$!4" /*This is for Security Token-Based authentication, when the client 
-    sends back the token to the server, the server verifies the token using the SECRETKEY*/
-}
