66aa68d5d9b42802839b8d75a37805e02b3d0d97
Jorge Fergusson <jorgefergusson@gmail.com>
2023-08-07 14:43:00 -0400

    Appointment service - updateAppointment

--- src/appointmentService.js
+++ src/appointmentService.js
@@ -1,21 +1,28 @@
 var appointmentModel = require("./appointmentModel");
 /*
 Create an Appointment
-*/ 
+*/
 module.exports.saveAppointmentInfoService = (appointmentDetails) => {
-    return new Promise(function savePatientInfoFun(resolve, reject) {
-        var appointmentModelData = new appointmentModel();
-        appointmentModelData._idDate = appointmentDetails._idDate;
-        appointmentModelData.Date = appointmentDetails.Date;
-        appointmentModelData.startTime = appointmentDetails.startTime;
-        appointmentModelData.endTime = appointmentDetails.endTime;
-        appointmentModelData.isBooked = appointmentDetails.isBooked;
-        patientModelData.save(function resultHandle(error, result) {
-            if (error) {
-              reject(false);
-            } else {
-              resolve(true);
-            }
-        });
+  return new Promise(function saveAppointmentInfoFunctionality(
+    resolve,
+    reject
+  ) {
+    var appointmentModelData = new appointmentModel();
+    appointmentModelData._idDate = appointmentDetails._idDate;
+    appointmentModelData.Date = appointmentDetails.Date;
+    appointmentModelData.startTime = appointmentDetails.startTime;
+    appointmentModelData.endTime = appointmentDetails.endTime;
+    appointmentModelData.isBooked = appointmentDetails.isBooked;
+    patientModelData.save(function resultHandle(error, result) {
+      if (error) {
+        reject(false);
+      } else {
+        resolve(true);
+      }
     });
+  });
+};
+
+module.exports.appointmentUpdateDetailsService = (appointmentUpdateDetails) => {
+  return new Promise(function appointmentUpdate(resolve, reject) {});
 };
