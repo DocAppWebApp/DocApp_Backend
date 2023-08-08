abc3e5eb267f81a388c5223ea3da5ae356c9a960
Jorge Fergusson <jorgefergusson@gmail.com>
2023-08-07 22:03:18 -0400

    Routes added for appointments

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,21 @@
 var express = require("express");
 var patientController = require("../src/patientController");
 var physicianController = require("../src/physicianController");
+var scheduleController = require("../src/scheduleController");
+var appointmentController = require("../src/appointmentController");
 const router = express.Router();
 
-router.route("/patient/save").post(patientController.savePatientInfoController);
+//Patients
+router
+  .route("/patient/save")
+  .post(patientController.savePatientInfoController);
 
 router
   .route("/patient/login")
   .post(patientController.loginPatientInfoController);
 
+
+//Physicians
 router
   .route("/physician/save")
   .post(physicianController.savePhysicianInfoController);
@@ -16,5 +23,19 @@ router
 router
   .route("/physician/login")
   .post(physicianController.loginPhysicianInfoController);
+
+
+//Appointments
+router
+  .route("/appointment/save")
+  .post(appointmentController.saveAppointmentInfoController);
+
+router
+  .route("/appointment/search")
+  .post(appointmentController.UpdateAppointmentInfoController);
+
+router
+  .route("/appointment/list")
+  .post(appointmentController.SearchAppointmentInfoController);
 
 module.exports = router;
